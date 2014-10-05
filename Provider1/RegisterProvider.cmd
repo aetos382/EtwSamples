5f2951367fa34a2e60e5580f11950e09b2411ddb
aetos <aetos@aerie.jp>
2014-10-06 02:16:06 +0900

    add icacls

--- Provider1/RegisterProvider.cmd
+++ Provider1/RegisterProvider.cmd
@@ -6,6 +6,7 @@ pushd "%~dp0"
 set manifest=%~dp0ProviderManifest.man
 set resource=%~dp0Provider1.exe
 
+icacls "%resource%" /grant "NT AUTHORITY\Local Service":RX /Q
 wevtutil.exe im "%manifest%" /rf:"%resource%" /mf:"%resource%" /pf:"%resource%"
 
 popd
