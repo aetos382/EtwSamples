b42167c686ff4d7632f0eb254947205ca2608c69
aetos <aetos@aerie.jp>
2014-10-06 18:31:40 +0900

    update provider0

--- Provider0/trace0.cmd
+++ Provider0/trace0.cmd
@@ -3,11 +3,11 @@
 setlocal
 pushd "%~dp0"
 
+set name=trace_test0
+set guid={FF5D07C6-03BA-481F-B07F-97CB8A51FADF}
+
 if exist "%~dp0%name%*.etl" del "%~dp0%name%*.etl"
 if exist "%~dp0%name%*.xml" del "%~dp0%name%*.xml"
-
-set name=trace_test
-set guid={FF5D07C6-03BA-481F-B07F-97CB8A51FADF}
 
 logman create trace %name% -p %guid% -o "C:\Users\Kazu\Documents\Visual Studio\Projects\EtwSamples\Debug\%name%.etl" -ets
 
