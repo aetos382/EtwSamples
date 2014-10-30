43e1ebe8ff77c7f3532c8d183d0324db26912c3d
aetos <aetos@aerie.jp>
2014-10-30 18:53:08 +0900

    add Provider3 and Consumer3

---
+++ Consumer3/Consumer3.cpp
@@ -0,0 +1,11 @@
+// Consumer3.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "../Provider3/ProviderManifest3.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
---
+++ Provider3/Provider3.cpp
@@ -0,0 +1,11 @@
+// Provider3.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "ProviderManifest3.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
